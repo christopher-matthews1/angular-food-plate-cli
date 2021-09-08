58c609c4fdcb6715d6f79f71616266468c5e1329
christopher-matthews1 <christopher.matthews@jbhunt.com>
2021-09-08 12:30:15 -0400

    Add logic in goals component and edit nav template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ExercisesComponent } from "./exercises/exercises.component";
 import { FarmersMarketComponent } from "./farmers-market/farmers-market.component";
 import { foodGroupsRoutes } from "./food-groups/food-groups.routing";
 import { FoodComponent } from "./food/food.component";
+import { GoalsComponent } from "./goals/goals.component";
 import { PlateComponent } from "./plate/plate.component";
 import { RegisterComponent } from "./register/register.component";
 import { LeaveRegisterGuardService } from "./services/leave-register-guard.service";
@@ -27,6 +28,7 @@ const routes: Routes = [
             {path: 'farmers-market', component: FarmersMarketComponent},
             {path: 'exercises', component: ExercisesComponent},
             {path: 'nutritionInfo', component: FoodComponent},
+            {path: 'goals', component: GoalsComponent},
             {path: 'foodGroups',
                 loadChildren: () => import('./food-groups/food-groups.module')
                     .then(mod => mod.FoodGroupsModule)},
--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from 
+'@angular/forms';
 
 @Component({
   selector: 'fp-goals',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsComponent implements OnInit {
 
-  constructor() { }
+  goalForm: FormGroup;
+
+  createForm(): void {
+    this.goalForm = this.fb.group({
+      id: [''],
+      goalTitle: ['', Validators.required],
+      dealine: ['', Validators.required],
+      didIt: ['']
+    });
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
 }
